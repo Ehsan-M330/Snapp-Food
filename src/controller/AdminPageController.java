c6ac8b6d16d21e670dccb3f28685b97e32777bd5
MrThink26 <95047019+MrThink26@users.noreply.github.com>
2022-06-13 02:45:48 +0430

    Admin Page Done...

--- src/controller/AdminPageController.java
+++ src/controller/AdminPageController.java
@@ -1,13 +1,48 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.*;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import model.TypeOfPlace;
 import java.io.IOException;
 
 public class AdminPageController extends Helper {
 
+    @FXML
+    private Button addCafebutton;
+
+    @FXML
+    private Button addDeliverybutton;
+
+    @FXML
+    private Button addRestaurantbutton;
+
+    @FXML
+    private Button backbutton;
+
+    @FXML
+    void Style1(MouseEvent event) {
+        backbutton.setCursor(Cursor.HAND);
+    }
+
+    @FXML
+    void Style2(MouseEvent event) {
+        addRestaurantbutton.setCursor(Cursor.HAND);
+    }
+
+    @FXML
+    void Style3(MouseEvent event) {
+        addCafebutton.setCursor(Cursor.HAND);
+    }
+
+    @FXML
+    void Style4(MouseEvent event) {
+        addDeliverybutton.setCursor(Cursor.HAND);
+    }
 
     public void addRestaurant(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("..\\view\\addPlace.fxml"));
